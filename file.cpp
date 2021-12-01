b241c7d7bc189d4b46a51a1e37b2fadceb8d5263
Devaansh Kakeri <devaansh6@gmail.com>
2021-12-01 17:43:07 +0530

    Add files via upload

---
+++ file.cpp
@@ -0,0 +1,33 @@
+#pragma GCC optimize "trapv"
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+using namespace std;
+void solve();
+int main()
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("error.txt", "w", stderr);
+freopen("output.txt", "w", stdout);
+#endif
+
+int t=1;
+cin>>t;
+while(t--)
+{
+	solve();
+	cout<<"\n";
+}
+
+cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+return 0;
+}
+void solve()
+{
+	int i=69;
+	cout<<i;
+}
